66e8a2fc842bdd7a3d57525629a51a0a4f03071e
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-05-01 20:10:19 +0700

    feat: :sparkles: react-query basic setup

--- src/components/providers/app-provider.tsx
+++ src/components/providers/app-provider.tsx
@@ -1,11 +1,14 @@
+import QueryProvider from './query-provider'
+import ThemeProvider from './theme-provider'
 import { Toaster } from '../ui/sonner'
-import { ThemeProvider } from './theme-provider'
 
 export default function AppProvider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider defaultTheme='light' storageKey='ui-theme'>
-      {children}
-      <Toaster position='top-right' />
-    </ThemeProvider>
+    <QueryProvider>
+      <ThemeProvider defaultTheme='light' storageKey='ui-theme'>
+        {children}
+        <Toaster position='top-right' />
+      </ThemeProvider>
+    </QueryProvider>
   )
 }
