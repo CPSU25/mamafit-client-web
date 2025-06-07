f79d2e1e2e736a5145f6b851df51bebea1d9dc2b
Nguyễn Lê Phương Nam <henrynguyen1553@gmail.com>
2025-06-07 14:02:20 +0700

    fix: :bug: fix bug reload loop

--- src/lib/axios/axios.ts
+++ src/lib/axios/axios.ts
@@ -107,9 +107,7 @@ api.interceptors.response.use(
             authStore.clear()
 
             // Redirect to login nếu refresh fail
-            if (typeof window !== 'undefined') {
-              window.location.href = '/sign-in'
-            }
+            
 
             return Promise.reject(refreshError)
           }
